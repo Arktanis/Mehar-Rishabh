ac5962624e5faecf4472ae665792f4c331099a77
Executor Arktanis <93070986+Arktanis@users.noreply.github.com>
2023-03-10 15:15:12 +0000

    modified:   hack.py 	modified:   hello.py

--- hack.py
+++ hack.py
@@ -1,5 +1,4 @@
 import hello as h
-print("The First number is",h.x)
-print("The First number is",h.y)
-print("The Added number is\n")
-print(h.sum())
+print("The First number is-\t",h.x)
+print("The First number is-\t",h.y)
+print("The Added number is-\t",h.sum())
